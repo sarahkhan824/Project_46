e96285e32d36e9d97108b902ad4c896bb8be1e55
sarahkhan824 <73019932+sarahkhan824@users.noreply.github.com>
2021-03-17 18:10:21 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,16 @@
+(function (){
+  var environmentQuestions = [{
+   question : "What heppens to plastic waste?"  , 
+   options :["disintegrate","breaks in little peices", "recycle", "dumped in the oceans" ],
+   answer : 1 }, 
+   {
+    question : "What heppens to plastic waste?"  , 
+   options :["disintegrate","breaks in little peices", "recycle", "dumped in the oceans" ],
+   answer : 1 },    
+   { question : "What heppens to plastic waste?"  , 
+   options :["disintegrate","breaks in little peices", "recycle", "dumped in the oceans" ],
+   answer : 1 },
+    
+   
+}]
+})
